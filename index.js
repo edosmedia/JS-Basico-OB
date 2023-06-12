65e0bdba642ea1017a39e93785a9e3b59f77e8c7
Eduardo Medrano <eduardomedranop@gmail.com>
2023-06-12 07:43:05 -0400

    ejercicio 2

---
+++ index.js
@@ -0,0 +1,14 @@
+let lista = {
+          nombre: "Eduardo Medrano",
+          edad: 35,
+          desarrollador: true,
+          nacimiento: new Date("1987-08-12"),
+          libro_favorito: [
+                    {
+                              titulo: "steve jobs",
+                              autor: "steve jobs",
+                              fecha: new Date("2015-11-11"),
+                              url: "https://ejemplo.com/123345",
+                    },
+          ],
+};
